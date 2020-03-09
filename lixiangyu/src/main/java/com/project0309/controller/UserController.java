faaa85af6d0063d83958eac3b59d36ffab5a1569
rookie599 <1525755684@qq.com>
2020-03-09 21:40:26 +0800

    李翔宇0309提交作业2

--- lixiangyu/src/main/java/com/project0309/controller/UserController.java
+++ lixiangyu/src/main/java/com/project0309/controller/UserController.java
@@ -4,12 +4,15 @@ import com.project0309.entity.UserEntity;
 import com.project0309.service.IUserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.FieldError;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 import javax.servlet.http.HttpServletResponse;
+import javax.validation.Valid;
 import java.io.IOException;
 
 
@@ -25,8 +28,22 @@ public class UserController {
     }
 
     @RequestMapping("add")
-    public void addUser(@RequestBody UserEntity userEntity, HttpServletResponse response) throws IOException {
+    public void addUser(@RequestBody @Valid UserEntity userEntity, BindingResult result, HttpServletResponse response) throws IOException {
+        if (result.hasErrors()) {//判断验证是否有错误信息
+            //错误信息字符串
+            String str = "";
+            //遍历验证错误的集合，并且拼接错误信息
+            for (FieldError fieldError : result.getFieldErrors()) {
+                str += fieldError.getDefaultMessage() + "<br>";
+            }
+            //把错误信息字符串响应给浏览器
+            response.setContentType("text/html;charset=utf-8");
+            response.getWriter().print(str);
+            //跳出方法
+            return;
+        }
         userService.addUser(userEntity);
+        //成功则响应字符串ok
         response.getWriter().print("ok");
     }
 
--- lixiangyu/src/main/java/com/project0309/entity/UserEntity.java
+++ lixiangyu/src/main/java/com/project0309/entity/UserEntity.java
@@ -1,13 +1,18 @@
 package com.project0309.entity;
 
+import javax.validation.constraints.Min;
+import javax.validation.constraints.Pattern;
 
 /**
  * 用户实体类
  */
 public class UserEntity {
     private int id;
+    @Pattern(regexp = "[a-z]{3}", message = "用户名必须是三位的小写字母")
     private String userName;
+    @Pattern(regexp = "\\d{3}", message = "密码必须是三位数字")
     private String pwd;
+    @Min(value = 1, message = "年龄必须是数字")
     private int age;
 
     public int getId() {
